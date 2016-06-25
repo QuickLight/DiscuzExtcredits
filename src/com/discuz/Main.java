26eefd73b038a8cd24620e9a559d233b0a906c10
Remilia_miao <775659148@qq.com>
2016-06-25 15:00:16 +0800

    添加Main方法

---
+++ src/com/discuz/Main.java
@@ -0,0 +1,33 @@
+package com.discuz;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Administrator on 2016/6/25 0025.
+ */
+public class Main extends JavaPlugin{
+
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    public void saveConfig() {
+        super.saveConfig();
+    }
+
+    public void reloadConfig() {
+        super.reloadConfig();
+    }
+
+    public void saveDefaultConfig() {
+        super.saveDefaultConfig();
+    }
+
+    public void onDisable() {
+        super.onDisable();
+    }
+}
